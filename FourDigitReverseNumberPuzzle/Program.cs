397d6f65375b417fd6a8e70ec152a9c30282d87b
gushwell <gushwell@gmail.com>
2018-02-18 16:06:33 +0900

    add

---
+++ FourDigitReverseNumberPuzzle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FourDigitReverseNumberPuzzle {
+	class Program {
+		static void Main(string[] args) {
+			for (int n = 1000; n < 9999; n++) {
+				int r = ReverseNumber(n);
+				if (n != r && (n % r == 0) && (r > 1000))
+					Console.WriteLine(n);
+			}
+		}
+
+		private static int ReverseNumber(int n) {
+			int ans = 0;
+			while (n > 0) {
+				int remainder = n % 10;
+				ans = ans * 10 + remainder;
+				n /= 10;
+			}
+			return ans;
+		}
+	}
+}
