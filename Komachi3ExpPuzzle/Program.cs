2f9311e8a166739481beda2a89b84314ab452ba3
gushwell <gushwell@gmail.com>
2018-03-03 15:57:41 +0900

    add: Komachi3ExpPuzzle

--- Komachi3ExpPuzzle/Program.cs
+++ Komachi3ExpPuzzle/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 
 namespace Komachi3ExpPuzzle {
+	
 	class Program {
+		
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			var sol = new Solver();
+			foreach (var nums in sol.Solve()) {
+				string s = $"答えは、{nums[0]}{nums[1]}-{nums[2]} = " +
+					$"{nums[3]}{nums[4]}/{nums[5]} = {nums[6]}+{nums[7]}*{nums[8]} です。";
+				Console.WriteLine(s);
+			}
 		}
 	}
 }
