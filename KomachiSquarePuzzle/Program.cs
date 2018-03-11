62f4dc7cd554ccf05692ffe31004ac3754889e2d
gushwell <gushwell@gmail.com>
2018-03-11 20:39:34 +0900

    add KomachiSquarePuzzle

---
+++ KomachiSquarePuzzle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace KomachiSquarePuzzle {
+	
+	class Program {
+		static void Main(string[] args) {
+			foreach (var n in Enumerable.Range(100, 900).Where(n => IsKomachi(n)))
+				Console.WriteLine($"{n}の2乗 = {n * n}");
+		}
+
+		// LINQバージョン
+		static private bool IsKomachi(int n) {
+			string s = n.ToString() + (n * n).ToString();
+			return "123456789".All(c => s.Contains(c));
+		}
+
+		// 非LINQバージョン
+		static private bool IsKomachiWithoutLinq(int n) {
+			char[] array = (n.ToString() + (n * n).ToString()).ToArray();
+			Array.Sort(array);
+			var s = new string(array);
+			return s == "123456789";
+		}
+
+	}
+}
