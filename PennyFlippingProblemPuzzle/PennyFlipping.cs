e1bec2e44b3d858e38554c2852dd8d8754026865
gushwell <gushwell@gmail.com>
2018-11-17 13:16:33 +0900

    覆銭問題（Penny Flipping Problem）

---
+++ PennyFlippingProblemPuzzle/PennyFlipping.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PennyFlippingProblemPuzzle {
+	public class PennyFlipping {
+		private int _number;
+		// falseが表
+		private bool[] _disks;
+
+		public PennyFlipping(int n) {
+			_number = n;
+			_disks = new bool[n];
+		}
+
+
+		// 問題を解く 
+		public IEnumerable<(int, bool[])> Solve() {
+			int count = 0;
+			foreach (var n in Repeat(Enumerable.Range(1, _number))) {
+				Turn(n);
+				yield return (n, _disks.ToArray());
+				count++;
+				if (IsFin())
+					break;
+			}
+		}
+
+		// 1,2,3,...n,1,2,3,...n,1,2,3,...nを永遠に繰り返す
+		private IEnumerable<int> Repeat(IEnumerable<int> nums) {
+			while (true) {
+				foreach (var n in nums)
+					yield return n;
+			}
+		}
+
+		// 先頭N枚のコインをひっくり返す。
+		public void Turn(int n) {
+			var r = _disks.Take(n).Reverse().ToArray();
+			for (int i = 0; i < n; i++) {
+				_disks[i] = !r[i];
+			}
+		}
+
+		// 終了したかどうか
+		public bool IsFin() {
+			return _disks.All(d => d == false);
+		}
+	}
+}
---
+++ PennyFlippingProblemPuzzle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace PennyFlippingProblemPuzzle {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("コインの枚数は?");
+			var line = Console.ReadLine();
+			if (int.TryParse(line, out var number)) {
+				number = Math.Max(3, Math.Min(20, number));
+				var pf = new PennyFlipping(number);
+				var result = pf.Solve().ToArray();
+				foreach ((var n, var coins) in result) {
+					PrintDisks(n, coins);
+				}
+				Console.WriteLine($"{result.Length}手で元に戻る");
+			}
+		}
+
+		private static void PrintDisks(int count, bool[] coins) {
+			string s = string.Format("({0,2}枚) : ", count);
+			foreach (var d in coins) {
+				s += (d ? "1 " : "0 ");
+			}
+			Console.WriteLine(s); ;
+		}
+	}
+}
