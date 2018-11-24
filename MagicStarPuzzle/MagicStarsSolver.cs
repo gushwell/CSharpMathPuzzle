fbf26fbd0659d52eedbb1165a90c342b7319b4b9
gushwell <gushwell@gmail.com>
2018-11-24 14:31:42 +0900

    ナンプレ（数独）を解く

---
+++ MagicStarPuzzle/MagicStarsSolver.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace MagicStarPuzzle {
+	public class MagicStarsSolver {
+		public MagicStarsSolver() {
+		}
+	}
+}
---
+++ NumberPlaceSolver/Location.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NumberPlaceSolver {
+	class Location {
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public Location(int x, int y) {
+			X = x;
+			Y = y;
+		}
+	}
+}
